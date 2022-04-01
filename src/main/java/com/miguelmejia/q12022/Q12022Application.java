f820ee63555cd288e22c3eddc5ffb4cf42582a35
Miguel Mejía <mmejiabr@pichincha.com>
2022-04-01 09:49:49 -0500

    Big Bang

---
+++ src/main/java/com/miguelmejia/q12022/Q12022Application.java
@@ -0,0 +1,13 @@
+package com.miguelmejia.q12022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q12022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q12022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguelmejia/q12022/Q12022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelmejia.q12022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q12022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
